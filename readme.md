c41124d665d1bc32dadb6b307fab965b41e2ed07
Colin Bell <cobell206@gmail.com>
2021-03-13 22:42:36 -0700

    Added readme.md and finished project

---
+++ readme.md
@@ -0,0 +1,19 @@
+
+# Homework 10 Employee Card
+
+## Example video
+![Demo Video](homework_10_working.gif)
+https://drive.google.com/file/d/1n0XNqwCptlvolkoRQ0ijlyEKRSGBV6Ol/preview
+
+## Instructions
+### Initializing
+Run 'npm install;
+
+### Testing
+Run 'npm test'
+
+### Running html generator
+Run 'node index.js'
+Answer the questions provided
+Open 'index.html' to see website
+
--- test/employee.test.js
+++ test/employee.test.js
@@ -1,6 +1,6 @@
 const {Employee, Manager, Engineer, Intern} = require ('../employee_generator')
 
-
+// Test Manager class
 describe('NewManager', () => {
     it('Should create manager object', () => {
         const new_manager = new Manager('Person', 1, 'email', 1234)
@@ -10,8 +10,18 @@ describe('NewManager', () => {
         expect(new_manager.email).toEqual('email')
         expect(new_manager.office_number).toEqual(1234)
     })
+
+    it('Should return correct values from functions', () => {
+        const new_manager = new Manager('Person', 1, 'email', 'url')
+
+        expect(new_manager.getName()).toEqual('Person')
+        expect(new_manager.getId()).toEqual(1)
+        expect(new_manager.getEmail()).toEqual('email')
+        expect(new_manager.getRole()).toEqual('Manager')
+    })
 })
 
+// Test Enginer Class
 describe('NewEngineer', () => {
     it('Should create engineer object', () => {
         const new_engineer = new Engineer('Person', 1, 'email', 'url')
@@ -21,9 +31,20 @@ describe('NewEngineer', () => {
         expect(new_engineer.email).toEqual('email')
         expect(new_engineer.github).toEqual('url')
     })
+
+    it('Should return correct values from functions', () => {
+        const new_engineer = new Engineer('Person', 1, 'email', 'url')
+
+        expect(new_engineer.getName()).toEqual('Person')
+        expect(new_engineer.getId()).toEqual(1)
+        expect(new_engineer.getEmail()).toEqual('email')
+        expect(new_engineer.getRole()).toEqual('Engineer')
+        expect(new_engineer.getGithub()).toEqual('url')
+    })
 })
 
-describe('NewInter', () => {
+// Test Intern Class
+describe('NewIntern', () => {
     it('Should create intern object', () => {
         const new_intern = new Intern('Person', 1, 'email', 'school')
 
@@ -31,5 +52,35 @@ describe('NewInter', () => {
         expect(new_intern.id).toEqual(1)
         expect(new_intern.email).toEqual('email')
         expect(new_intern.school).toEqual('school')
+    })
+
+    it('Should return correct values from functions', () => {
+        const new_intern = new Intern('Person', 1, 'email', 'school')
+
+        expect(new_intern.getName()).toEqual('Person')
+        expect(new_intern.getId()).toEqual(1)
+        expect(new_intern.getEmail()).toEqual('email')
+        expect(new_intern.getRole()).toEqual('Intern')
+        expect(new_intern.getSchool()).toEqual('school')
+    })
+})
+
+// Test Employee Class
+describe('NewEmployee', () => {
+    it('Should create employee object', () => {
+        const new_employee = new Employee('Person', 1, 'email')
+
+        expect(new_employee.name).toEqual('Person')
+        expect(new_employee.id).toEqual(1)
+        expect(new_employee.email).toEqual('email')
+    })
+
+    it('Should return correct values from functions', () => {
+        const new_employee = new Employee('Person', 1, 'email')
+
+        expect(new_employee.getName()).toEqual('Person')
+        expect(new_employee.getId()).toEqual(1)
+        expect(new_employee.getEmail()).toEqual('email')
+        expect(new_employee.getRole()).toEqual('Employee')
     })
 })
